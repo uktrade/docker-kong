6cbba752906ab50f541198ca1d8572c3bb2750dc
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2021-08-19 16:39:00 +0100

    fix typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@
 export KONG_PG_HOST=$(echo $VCAP_SERVICES | jq ".postgres[0].credentials.host")
 export KONG_DATABASE=$(echo $VCAP_SERVICES | jq ".postgres[0].credentials.name")
 export KONG_PG_USER=$(echo $VCAP_SERVICES | jq ".postgres[0].credentials.username")
-export KONG_PG_PASSWORD=(echo $VCAP_SERVICES | jq ".postgres[0].credentials.password")
+export KONG_PG_PASSWORD=$(echo $VCAP_SERVICES | jq ".postgres[0].credentials.password")
 
 exec /_docker-entrypoint.sh $@

4bca51fb65d99bc1bf8920450ea41975d8e914bd
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2021-08-19 17:15:09 +0100

    fix db config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+export KONG_DATABASE=postgres
 export KONG_PG_HOST=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.host")
-export KONG_DATABASE=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.name")
 export KONG_PG_USER=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.username")
 export KONG_PG_PASSWORD=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.password")
 

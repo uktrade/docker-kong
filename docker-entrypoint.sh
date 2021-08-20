5b5a856b24990e1c0575a2e92a69d9a0fa044022
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2021-08-20 17:46:23 +0100

    add port

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,7 @@ export KONG_PG_DATABASE=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.
 export KONG_PG_HOST=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.host")
 export KONG_PG_USER=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.username")
 export KONG_PG_PASSWORD=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.password")
+export KONG_PG_PORT=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.port")
 export KONG_PG_SSL=on
 
 exec /_docker-entrypoint.sh $@

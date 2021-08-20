2260c0ad5f806645efff36fddcd01de8d627a743
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2021-08-20 13:28:47 +0100

    verify tls

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,6 @@ export KONG_DATABASE=postgres
 export KONG_PG_HOST=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.host")
 export KONG_PG_USER=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.username")
 export KONG_PG_PASSWORD=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.password")
+export KONG_PG_SSL=on
 
 exec /_docker-entrypoint.sh $@

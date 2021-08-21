6233d5060d7ed91f691e0104a4f35a81821e0b57
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2021-08-21 12:44:18 +0100

    debug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,4 +8,17 @@ export KONG_PG_PASSWORD=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.
 export KONG_PG_PORT=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.port")
 export KONG_PG_SSL=on
 
+echo "------------"
+echo "database"
+echo $KONG_PG_DATABASE
+echo "host"
+echo $KONG_PG_HOST
+echo "user"
+echo $KONG_PG_USER
+echo "password"
+echo $KONG_PG_PASSWORD
+echo "port"
+echo $KONG_PG_PORT
+echo "------------"
+
 exec /_docker-entrypoint.sh $@

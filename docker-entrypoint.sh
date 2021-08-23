6e27f3e44df33b0cd52f42b2df964155f3e2b433
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2021-08-23 16:31:09 +0100

    reinstate docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,18 +7,7 @@ export KONG_PG_USER=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.user
 export KONG_PG_PASSWORD=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.password")
 export KONG_PG_PORT=$(echo $VCAP_SERVICES | jq -r ".postgres[0].credentials.port")
 export KONG_PG_SSL=on
-
-echo "------------"
-echo "database"
-echo $KONG_PG_DATABASE
-echo "host"
-echo $KONG_PG_HOST
-echo "user"
-echo $KONG_PG_USER
-echo "password"
-echo $KONG_PG_PASSWORD
-echo "port"
-echo $KONG_PG_PORT
-echo "------------"
+export KONG_PG_SSL_VERIFY=on
+export KONG_LUA_SSL_TRUSTED_CERTIFICATE=/rds-combined-ca-bundle.pem
 
 exec /_docker-entrypoint.sh $@
